cae48311b07f8067fbd708555513028dca50faf9
Ivan Zhang <yzhang24@ualberta.ca>
2020-10-04 20:31:05 -0600

    lab 4

--- Team10/app/src/main/java/com/example/team10/Shape.java
+++ Team10/app/src/main/java/com/example/team10/Shape.java
@@ -4,4 +4,19 @@ public abstract class Shape {
     private int x;
     private int y;
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
