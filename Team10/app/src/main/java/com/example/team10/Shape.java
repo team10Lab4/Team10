79441e50699213e07fc7019c62f65801cc935556
ZjTan4 <32871093+ZjTan4@users.noreply.github.com>
2020-10-04 21:11:38 -0600

    Update Shape.java

--- Team10/app/src/main/java/com/example/team10/Shape.java
+++ Team10/app/src/main/java/com/example/team10/Shape.java
@@ -3,5 +3,5 @@ package com.example.team10;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR_1";
 }
