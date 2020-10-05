1f09db5eea74de9e4110e8931c8a03f3a6f491f1
Ethylene000 <1056183440@qq.com>
2020-10-05 10:45:51 +0800

    Update Shape.java

--- Team10/app/src/main/java/com/example/team10/Shape.java
+++ Team10/app/src/main/java/com/example/team10/Shape.java
@@ -3,20 +3,5 @@ package com.example.team10;
 public abstract class Shape {
     private int x;
     private int y;
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
